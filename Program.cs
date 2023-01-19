bd8e9f8b9145d667a428b0896288299bd8aa3d2d
Tomislav Klemić <tklemic@omega-software.hr>
2023-01-19 10:21:02 +0100

    Added Rhetos dashboard

--- Program.cs
+++ Program.cs
@@ -7,15 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.CustomSchemaIds(type => type.ToString());
+});
 
 builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHostBuilder
         .ConfigureRhetosAppDefaults()
         .UseBuilderLogProviderFromHost(serviceProvider)
         .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
     .AddAspNetCoreIdentityUser()
-    .AddHostLogging();
-    
+    .AddHostLogging()
+    .AddDashboard()
+    .AddRestApi(o =>
+    {
+        o.BaseRoute = "rest";
+        o.GroupNameMapper = (conceptInfo, controller, oldName) => "v1";
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +39,11 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseRhetosRestApi();
+
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+    app.MapRhetosDashboard();
 
 app.Run();
