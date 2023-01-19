77549313447e3229fb1e00415692f19eaa4ca0eb
tklemic <tklemic@TKLEMIC-LAP>
2023-01-19 08:35:13 +0100

    Add project files.

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rhetos;
+using Rhetos.Processing;
+using Rhetos.Processing.DefaultCommands;
+
+[Route("Demo/[action]")]
+public class DemoController : ControllerBase
+{
+    private readonly IProcessingEngine processingEngine;
+    private readonly IUnitOfWork unitOfWork;
+
+    public DemoController(IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<IUnitOfWork> unitOfWork)
+    {
+        this.processingEngine = processingEngine.Value;
+        this.unitOfWork = unitOfWork.Value;
+    }
+
+    [HttpGet]
+    public string ReadBooks()
+    {
+        var readCommandInfo = new ReadCommandInfo { DataSource = "Bookstore.Book", ReadTotalCount = true };
+        var result = processingEngine.Execute(readCommandInfo);
+        return $"{result.TotalCount} books.";
+    }
+
+    [HttpGet]
+    public string WriteBook()
+    {
+        var newBook = new Bookstore.Book { Title = "NewBook" };
+        var saveCommandInfo = new SaveEntityCommandInfo { Entity = "Bookstore.Book", DataToInsert = new[] { newBook } };
+        processingEngine.Execute(saveCommandInfo);
+        unitOfWork.CommitAndClose(); // Commits and closes database transaction.
+        return "1 book inserted.";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Rhetos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHostBuilder
+        .ConfigureRhetosAppDefaults()
+        .UseBuilderLogProviderFromHost(serviceProvider)
+        .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
+    .AddAspNetCoreIdentityUser()
+    .AddHostLogging();
+    
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
