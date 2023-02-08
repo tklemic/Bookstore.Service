94b1782f606dcbebaabdd3838a0adf1db3fcbfb7
Tomislav Klemić <tklemic@omega-software.hr>
2023-02-08 10:56:02 +0100

    Kompletiran Day 7, tj. svih 7 dana Rhetos edukacije.

---
+++ MailNotifications/IMailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Bookstore
+{
+    public interface IMailSender
+    {
+        void SendMail(string message, List<string> emailAddresses);
+    }
+}
---
+++ RandomBookGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Rhetos.Dom.DefaultConcepts;
+using System;
+using System.Linq;
+
+namespace Bookstore.Service
+{
+    public class RandomBookGenerator
+    {
+        public static void InsertBooks(Common.DomRepository repository, int? numberOfBooks)
+        {
+            var books = Enumerable.Range(0, numberOfBooks.Value)
+                .Select(x => new Book { Title = Guid.NewGuid().ToString(), Code = Guid.NewGuid().ToString() }); // Random title.
+            repository.Bookstore.Book.Insert(books);
+        }
+    }
+}
